2496d993ffa5ed792db90c4b89693d6ef5993fb6
Stephen Fluin <stephen.fluin@gmail.com>
2025-05-24 14:08:50 -0500

    fix: remove prerendering
    cloud build doesn't have access to cloud sql, so I can't prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
   { path: 'r/:id', renderMode: RenderMode.Client },
 ];
