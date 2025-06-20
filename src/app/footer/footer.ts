d01dee3dd918b0136cd8df284e247aa53081043f
Stephen Fluin <stephen.fluin@gmail.com>
2025-06-20 02:50:13 -0500

    feat: render version number in footer

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import versionJson from '../../../worker/version.json';
 @Component({
   selector: 'app-footer',
   imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrls: ['./footer.scss'],
 })
-export class Footer {}
+export class Footer {
+  version = versionJson.version;
+}
